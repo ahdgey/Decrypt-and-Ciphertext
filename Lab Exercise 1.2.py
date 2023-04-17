f66f1863a0c36307d45c55c6eecdb265c7c2d053
Alexza Jean Catanoy <alexzacatanoy@gmail.com>
2023-04-17 11:31:21 +0800

    First Commit

---
+++ Lab Exercise 1.2.py
@@ -0,0 +1,49 @@
+# Alexza Jean R. Catanoy
+# BSCPE 1-4
+# Lab Exercise No. 1.2
+
+#Title
+print("\033[0;36m*" * 70)
+print("\033[1;97mDecrypt a Encrypted Text".center(77)) 
+print("\033[0;36m*" * 70)
+
+print("\033[1;32m\nHello! Your programmers name is Alexza Jean.")
+print("\033[1;32m\nShe's from BSCPE 1-4.")
+print("-" * 70)
+
+# Request for users encrypted input and then save it
+input_str = input("\033[0;33m\nKindly type in a encrypted text that I can decrypt?\n")
+output_str = ""
+
+# Put every character and its corresponding value
+for i in range(len(input_str)):
+#   if *, change a
+    if input_str[i] == "*":
+        output_str += "a"
+#   if &, change e
+    elif input_str[i] == "&":
+        output_str += "e"
+#   if #, change i
+    elif input_str[i] == "#":
+        output_str += "i"
+#   if +, change o
+    elif input_str[i] == "+":
+        output_str += "o"
+#   if !, change u
+    elif input_str[i] == "!":
+        output_str += "u"
+    else:
+        output_str += input_str[i]
+
+print("=" * 70)
+
+# Print the output of this program
+print("\033[0;35m\nThe Encrypted Text is: " + input_str)
+print("\033[0;35m\nThe Decrypted Text is: " + output_str)
+print("_" * 70)
+
+#End the program
+print("\033[0;31m\nThe program ends here, see you next time!")
+print("~" * 70)
+
+
---
+++ Lab Exercise 1.3.py
@@ -0,0 +1,37 @@
+# Alexza Jean R. Catanoy
+# BSCPE 1-4
+# Lab Exercise No. 1.3
+
+#Title
+print("\033[0;36m*" * 70)
+print("\033[1;97mThe Vigenère Cipher".center(77)) 
+print("\033[0;36m*" * 70)
+
+print("\033[1;32m\nHello! Your programmers name is Alexza Jean.")
+print("\033[1;32m\nShe's from BSCPE 1-4.")
+print("-" * 70)
+
+
+# Request for users message and key input
+message = input("\033[0;33m\nKindly type in the message(all uppercase letters, no spaces): ")
+key = input("\033[0;33m\nKindly type in the keyword(all uppercase letters): ")
+
+# Apply ASCII code to the key to convert it into the appropriate letter values of 0 to 25.
+key_map = [ord(i) - 65 for i in key]
+
+# Implement the message encryption
+ciphertext = ""
+for i in range(len(message)):
+    encrypted_num = (ord(message[i]) - 65 + key_map[i % len(key_map)]) % 26
+    ciphertext += chr(encrypted_num + 65)
+
+print("=" * 70)
+
+# Print the output of this program
+print("\033[0;35m\nThe ciphertext is: " + ciphertext)
+print("_" * 70)
+
+# End the program
+print("\033[0;31m\nThe program ends here, see you next time!")
+print("~" * 70)
+
